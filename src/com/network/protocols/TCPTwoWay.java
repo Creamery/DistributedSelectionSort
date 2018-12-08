83e8457bcdceeaff4bd65dccc6b3e44391cd8e02
Candy <candy.espulgar@gmail.com>
2018-12-09 01:35:58 +0900

    Update TCPTwoWay.java

--- src/com/network/protocols/TCPTwoWay.java
+++ src/com/network/protocols/TCPTwoWay.java
@@ -210,13 +210,15 @@ public class TCPTwoWay extends Thread {
 		else {
 			System.out.println("As Client");
 			try {
+				
+				Socket socket = new Socket(this.getClientIP(), this.getPort());
 				BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
 	            // sending to client (pwrite object)
-				OutputStream ostream = this.getTcpClientSocket().getOutputStream(); 
+				OutputStream ostream = socket.getOutputStream(); 
 				PrintWriter pwrite = new PrintWriter(ostream, true);
 	
 	            // receiving from server ( receiveRead  object)
-				InputStream istream = this.getTcpClientSocket().getInputStream();
+				InputStream istream = socket.getInputStream();
 				BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 	
 				System.out.println("Start the chitchat, type and press Enter key");
