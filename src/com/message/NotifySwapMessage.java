c64228b028df7d08c47ae529d044012e9322fbbc
Neil Romblon <neilromblon@live.com>
2018-12-09 16:45:53 +0800

    toString andParse

--- src/com/message/NotifySwapMessage.java
+++ src/com/message/NotifySwapMessage.java
@@ -21,4 +21,18 @@ public class NotifySwapMessage implements Serializable {
     public int getIndexB(){
         return this.indexB;
     }
+
+    public String toString(){
+        return "SWAP:"+indexA+"-"+indexB;
+    }
+
+    public static NotifySwapMessage parseString(String s){
+        if(s.contains("SWAP:")){
+            s = s.substring(5);
+            String[] tokens = s.split("-");
+            return new NotifySwapMessage(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+        }
+        else
+            return null;
+    }
 }
